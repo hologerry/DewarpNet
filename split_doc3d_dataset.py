f6ca80f8aa65ac632006fb2c5b4e4265545539cb
Gerry Gao <gerry_gao@apple.com>
2020-05-23 18:56:32 +0000

    fix typo

--- split_doc3d_dataset.py
+++ split_doc3d_dataset.py
@@ -7,7 +7,7 @@ for i in range(1, 17):
     names = os.listdir(cur_path)
     for name in names:
         curline = str(i) + '/' + name.split('.')[0] + '\n'
-        train_f.write(name)
+        train_f.write(curline)
 
 
 val_f = open(os.path.join(root_dir, 'val.txt'), 'w')
@@ -16,7 +16,7 @@ for i in range(17, 22):
     names = os.listdir(cur_path)
     for name in names:
         curline = str(i) + '/' + name.split('.')[0] + '\n'
-        val_f.write(name)
+        val_f.write(curline)
 
 
 train_f.close()
